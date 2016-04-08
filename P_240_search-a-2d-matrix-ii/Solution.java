0767abf10f338147765503a4659efd23dd7a0bae
YiZhou <dugu9sword@163.com>
2016-04-08 12:19:57 +0800

    better solution

--- P_240_search-a-2d-matrix-ii/Solution.java
+++ P_240_search-a-2d-matrix-ii/Solution.java
@@ -1,3 +1,33 @@
+/*
+ * We start search the matrix from top right corner, 
+ * initialize the current position to top right corner, 
+ * if the target is greater than the value in current position, 
+ * then the target can not be in entire row of current position because the row is sorted, 
+ * if the target is less than the value in current position, 
+ * then the target can not in the entire column because the column is sorted too. 
+ * We can rule out one row or one column each time, 
+ * so the time complexity is O(m+n).
+ * 
+ */
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int x,y;
+        if((x=matrix.length)==0 || (y=matrix[0].length)==0)
+            return false;
+        int i=0,j=y-1;
+        while(i<x && j>=0){
+            if(matrix[i][j]==target)
+                return true;
+            else if(matrix[i][j]>target)
+                j--;
+            else
+                i++;
+        }
+        return false;
+    }
+}
+
+/*
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if(matrix.length==0 || matrix[0].length==0)
@@ -51,3 +81,4 @@ public class Solution {
         return search(matrix,x1,x2,y1,y2,target);
     }
 }
+*/
